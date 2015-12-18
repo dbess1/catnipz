055f27742436174641a24a52c282c0402ec56c14
Delante Bess <dbess@g.harvard.edu>
2015-12-18 02:41:39 +0000

    Changed the app/models/microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151217023153) do
-=======
-ActiveRecord::Schema.define(version: 20151217221329) do
->>>>>>> user-microposts
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
   end
 
-=======
-    t.string   "picture"
-  end
-
-  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
->>>>>>> user-microposts
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
